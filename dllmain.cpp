72fdb29c99b6c057b68db6936e49c00fdf1e74dc
Diavolo <es597@sussex.ac.uk>
2020-12-22 11:57:09 +0100

    added other dvar

--- dllmain.cpp
+++ dllmain.cpp
@@ -15,17 +15,19 @@ DWORD WINAPI EntryPoint(LPVOID _arguments)
     Dvar_LockDvar("cg_crosshairDynamic", 0);
     Dvar_LockDvar("cg_drawCrosshair", 1);
     Dvar_LockDvar("ui_drawCrosshair", 1);
+    Dvar_LockDvar("fx_draw", 0);
 
     Dvar_LockDvar("g_hardcore", false);
     Dvar_LockDvar("ui_hud_hardcore", false);
     Dvar_LockDvar("compassPortableRadarRadius", 850.0f);
     Dvar_LockDvar("compassPortableRadarSweepTime", 2000);
     Dvar_LockDvar("compassPortableRadarMinVelocity", 500.0f);
+    Dvar_LockDvar("compassRadarPingFadeTime", 4.0f);
 
     dvar_t* dvar_1 = Dvar_FindDvar("cg_fov", true);
     Dvar_LockDvar(dvar_1, 100.0f);
-    dvar_t* dvar_2 = Dvar_FindDvar("ui_debugMode", true);
-    dvar_2->current = true;
+    //dvar_t* dvar_2 = Dvar_FindDvar("ui_debugMode", true);
+    //dvar_2->current = true;
     dvar_t * dvar_3 = Dvar_FindDvar("ragdoll_debug", true);
     dvar_3->current = true;
     dvar_t* dvar_4 = Dvar_FindDvar("radius_damage_debug", true);
